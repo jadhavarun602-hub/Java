d3850d2b41fe1d0c3bfb6ce7c06028f208269bc8
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-12-05 01:34:34 +0530

    loops_Q2

---
+++ LOOPS_Statements/loops_ex/Loops_2.java
@@ -0,0 +1,15 @@
+package loops_ex;
+
+// Print numbers from 100 to 1 using a loop.
+
+public class Loops_2 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 100; i >= 1; i--) {
+			System.out.println(i);
+		}
+
+	}
+
+}
