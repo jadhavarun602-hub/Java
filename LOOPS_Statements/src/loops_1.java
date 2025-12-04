bfc412a17c8b8459383c6c1e82eee54fc776bc35
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-12-04 21:14:30 +0530

    loops_Q1

---
+++ LOOPS_Statements/src/loops_1.java
@@ -0,0 +1,11 @@
+// Print numbers from 1 to 100 using a loop.
+
+public class loops_1 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100 ; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
