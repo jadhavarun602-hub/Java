c9da62f5aef14c7c61149ceffd07a64d36ce278e
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-12-05 12:34:59 +0530

    loops_Q6

---
+++ LOOPS_Statements/loops_ex/Loops_6.java
@@ -0,0 +1,21 @@
+package loops_ex;
+
+// Calculate the sum of all even numbers from 1 to 100 using a loop.
+
+public class Loops_6 {
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		
+		for(int i = 1; i <= 100; i++) {
+			
+			if(i % 2 == 0) {
+				sum += i;
+			}
+		}
+		
+		System.out.println("sum of all even numbers: "+sum);
+		
+	}
+
+}
