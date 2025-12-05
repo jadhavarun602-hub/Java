02c4e979267b2edd7746f99da114f4c8e58abdfd
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-12-05 12:16:26 +0530

    loops_Q3

---
+++ LOOPS_Statements/loops_ex/Loops_3.java
@@ -0,0 +1,18 @@
+package loops_ex;
+
+// Print all even numbers between 1 and 50 using a loop.
+
+public class Loops_3 {
+
+	public static void main(String[] args) {
+		
+		
+		for(int i = 1; i <= 50; i++) {
+			
+			if( i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
