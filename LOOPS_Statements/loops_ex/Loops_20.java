7615c778710d46cd4f1ff418a28037ed33cf29b3
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-12-07 16:54:52 +0530

    loops_Q20

---
+++ LOOPS_Statements/loops_ex/Loops_20.java
@@ -0,0 +1,34 @@
+package loops_ex;
+
+import java.util.Scanner;
+
+// 	write a given number factorial
+
+public class Loops_20 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc =  new Scanner(System.in);
+		System.out.print("Enter a Num: ");
+		
+		int num = sc.nextInt();
+		
+		int fact = 1;
+		
+		if(num < 1) {
+			System.out.println("Please enter less than 1 number...");
+			return;
+		}
+		
+		for(int i = 1; i <= num; i++) {
+			
+			fact *= i;
+			
+		}
+		
+		System.out.println("Given number Factorial is: "+fact);
+
+	}
+
+}
