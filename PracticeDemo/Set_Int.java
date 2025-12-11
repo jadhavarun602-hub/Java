015dac53769674eea341af7ea57e9e6ac4b401d9
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-12-11 22:37:16 +0530

    set peek ex

---
+++ PracticeDemo/Set_Int.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+public class Set_Int {
+
+	public static void main(String[] args) {
+		
+		
+		Stack<String> book = new Stack<>();
+		
+		book.add("Bhagavat Gita");
+		book.add("Marathi");
+		book.add("Hindi");
+		book.add("English");
+		
+		System.out.println(book.peek());
+		System.out.println(book);
+
+	}
+
+}
