11ed7fbb7de481d30cd9e31a027a932962ddbce1
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-12-13 15:49:51 +0530

    method_Q19

--- Methods/methods_ex/Method_19.java
+++ Methods/methods_ex/Method_19.java
@@ -3,34 +3,27 @@ package methods_ex;
 //Write a method to find the maximum element in an array.
 
 public class Method_19 {
+	
+	public static int findMax(int [] arr) {
+		
+		int max = 0;
+		
+		for(int i = 0; i < arr.length; i++ ) {
+			
+			if(arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		return max;
+	}
 
 	public static void main(String[] args) {
 		
 		int arr [] = {23,55,8,76,23,86,33,10};
 		
-		int max = 0;
+		int result = findMax(arr);
 		
-		
-		
-		for(int num : arr) {
-			
-			System.out.print(num+" ");	
-		
-		}
-		
-		System.out.println();
-		
-		
-		for(int i = 0; i < arr.length; i++) {
-			
-				if(arr[i] > max) {
-				 max = arr[i];
-					
-			}
-			
-		}
-		
-		System.out.print("maximum element is array: "+max);
+		System.out.print("maximum element in array: "+result);
 		
 	}
 
---
+++ Methods/methods_ex/Method_20.java
@@ -0,0 +1,13 @@
+package methods_ex;
+
+//Write a method to count vowels in a string.
+
+public class Method_20 {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
