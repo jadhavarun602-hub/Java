803c4920f1a827b922640eeb1852d9dc7bc60f59
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-12-13 16:18:28 +0530

    method_Q21

---
+++ Methods/methods_ex/Method_21.java
@@ -0,0 +1,34 @@
+package methods_ex;
+
+//Write a method to find the frequency of a character in a string.
+
+public class Method_21 {
+	
+	public static int findFrequency(String n, char tar) {
+		
+		int count = 0;
+		
+		for(int i = 0; i < n.length(); i++) {
+			
+			if(n.charAt(i) == tar) {
+				count++;
+			}
+		}
+		
+		return count;
+	}
+
+    
+    
+    public static void main(String[] args) {
+    	
+    	String text = "my name is arun jadhav";
+    	char ch = 'a';
+    	
+    	int find = findFrequency(text,ch);
+    	
+    	System.out.println("Frequency of '" + ch + "': " + find);
+
+    }
+}
+
