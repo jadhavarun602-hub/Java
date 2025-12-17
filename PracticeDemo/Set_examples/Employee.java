d1f480ab6b2a95c6644589bd286105df3b952a96
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-12-17 22:39:12 +0530

    Comparable example

---
+++ PracticeDemo/Set_examples/Employee.java
@@ -0,0 +1,46 @@
+package Set_examples;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Employee implements Comparable<Employee> {
+	String name;
+	int salary;
+	
+	public Employee(String name,int salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+	
+	@Override
+	public int compareTo(Employee o) {
+		
+		return Integer.compare(this.salary, o.salary);
+	}
+	
+
+	public static void main(String[] args) {
+		
+		
+		List<Employee> emp = new ArrayList<>();
+		
+		emp.add(new Employee("Arun",50000));
+		emp.add(new Employee("Rahul",35000));
+		emp.add(new Employee("Nandkumar",75000));
+		emp.add(new Employee("Priya",50000));
+		emp.add(new Employee("Pooja",45000));
+		
+		Collections.sort(emp);
+		
+		for(Employee n : emp) {
+			System.out.println(n.name+" : "+n.salary);
+		}
+		
+		
+		
+		
+
+	}
+
+}
