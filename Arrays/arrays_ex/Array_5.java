158f87d3ba6a8250bb866535ac6c248b223dd894
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-12-20 18:17:58 +0530

    array_Q5

---
+++ Arrays/arrays_ex/Array_5.java
@@ -0,0 +1,24 @@
+package arrays_ex;
+
+//Find the largest element in an array.
+
+public class Array_5 {
+
+	public static void main(String[] args) {
+		
+		int [] ele = {12,54,87,45,23,11,45,5};
+		
+		int larg = ele[0];
+		
+		for(int i = 0; i < ele.length; i++) {
+			
+			if(larg < ele[i]) {
+				larg = ele[i];
+			}
+		}
+		
+		System.out.println("Largest element in array: "+larg);
+
+	}
+
+}
