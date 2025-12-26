22a6d19f5673d5006082a0f5334d76feeb091db7
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-12-26 23:42:10 +0530

    string_Q1

---
+++ Strings/string_ex/String_1.java
@@ -0,0 +1,15 @@
+package string_ex;
+
+// Create a string and print its length.
+
+public class String_1 {
+
+	public static void main(String[] args) {
+		
+		String name = "Arun Jadhav";
+		
+		System.out.println("Lenght of String: "+name.length());
+
+	}
+
+}
