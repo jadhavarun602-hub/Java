cd15bef3653bc1479843668b16992d3035ef2418
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-12-30 19:52:10 +0530

    string_Q19

--- Strings/string_ex/String_19.java
+++ Strings/string_ex/String_19.java
@@ -1,9 +1,31 @@
 package string_ex;
 
+// Reverse each word in a sentence.
+
 public class String_19 {
+	
+	public static void reverseWord(String str) {
+		
+		String [] words = str.trim().split(" ");
+		
+		for(int i = 0; i < words.length; i++) {
+			
+			String ch = words[i];
+			
+			
+			for(int j = ch.length()-1; j >= 0; j--) {
+				
+				System.out.print(ch.charAt(j));
+			}
+			
+			System.out.print(" ");
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String word = "I AM FROM PUNE";
+		
+		reverseWord(word);
 
 	}
 
