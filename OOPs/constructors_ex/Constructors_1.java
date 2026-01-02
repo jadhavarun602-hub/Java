eadf8546a0a3877401cd94731256abfc9fa9437d
jadhavarun602-hub <jadhavarun602@gmail.com>
2026-01-02 10:10:35 +0530

    Create a class with default constructor

---
+++ OOPs/constructors_ex/Constructors_1.java
@@ -0,0 +1,22 @@
+package constructors_ex;
+
+// Create a class with default constructor.
+
+class Car {
+	
+	Car (){
+		System.out.println("Hey i am constructor...");
+	}
+}
+
+public class Constructors_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Car car = new Car();
+		
+
+	}
+
+}
