aed73feb165aa8471562aff404eb2cbccfd5102a
jadhavarun602-hub <jadhavarun602@gmail.com>
2026-01-02 10:16:05 +0530

    Create a class with parameterized constructor

---
+++ OOPs/constructors_ex/Constructors_2.java
@@ -0,0 +1,25 @@
+package constructors_ex;
+
+// Create a class with parameterized constructor.
+
+class Student {
+	
+	String name;
+	int roll;
+	
+	Student(String name, int roll){
+		System.out.println("Name: "+name);
+		System.out.println("Roll: "+roll);
+	}
+}
+
+public class Constructors_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student s1 = new Student("Arun", 111);
+
+	}
+
+}
