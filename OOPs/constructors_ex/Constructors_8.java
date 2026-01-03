bb3530ae40a88a7fb0142d34071f8b2c1046f0f7
jadhavarun602-hub <jadhavarun602@gmail.com>
2026-01-03 13:40:47 +0530

    Call one constructor from another using this().

--- OOPs/constructors_ex/Constructors_8.java
+++ OOPs/constructors_ex/Constructors_8.java
@@ -1,9 +1,42 @@
 package constructors_ex;
 
+// Call one constructor from another using this().
+
+class Person {
+	
+	String gender;
+	String name;
+	int age;
+	String city;
+	
+	Person(String gender){
+		this.gender = gender;
+	}
+	
+	
+	Person(String gender,String name,int age,String city){
+		this(gender);
+		this.name = name;
+		this.age = age;
+		this.city = city;
+		
+		System.out.println("Gender: "+gender);
+		System.out.println("Name: "+name);
+		System.out.println("Age: "+age);
+		System.out.println("City: "+city);
+		
+	}
+	
+	
+}
+
 public class Constructors_8 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Person p1 = new Person("Female");
+		Person p2 = new Person("Female","Pooja",21,"Pune");
 
 	}
 
