88ec6a89e2c6c1970d70463b77e20028e54d814e
jadhavarun602-hub <jadhavarun602@gmail.com>
2026-01-03 12:56:28 +0530

    Use this keyword inside constructor

---
+++ OOPs/constructors_ex/Constructor_7.java
@@ -0,0 +1,35 @@
+package constructors_ex;
+
+// Use this keyword inside constructor.
+
+class Id{
+	
+	String name;
+	String address;
+	String DOB;
+	Long number;
+	
+	Id(String name,String address,String DOB,long number){
+		
+		this.name = name;
+		this.address = address;
+		this.DOB = DOB;
+		this.number = number;
+		
+		System.out.println("Name: "+this.name);
+		System.out.println("Address: "+this.address);
+		System.out.println("DOB: "+this.DOB);
+		System.out.println("Contact: "+this.number);
+	}
+}
+
+public class Constructor_7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Id id1 = new Id("Arun","Pargaon","01/05/2004",8830347948l);
+
+	}
+
+}
