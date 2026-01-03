4120576a1257c83c63165228e4ba5ddfea9c6cea
jadhavarun602-hub <jadhavarun602@gmail.com>
2026-01-03 10:46:41 +0530

    Initialize variables using constructor.

---
+++ OOPs/constructors_ex/Class_Object_3.java
@@ -0,0 +1,34 @@
+package constructors_ex;
+
+// Initialize variables using constructor.
+
+class Student_1 {
+	
+	String name;
+	String std;
+	int roll;
+	
+	Student_1(String name,String std,int roll){
+		this.name = name;
+		this.std = std;
+		this.roll = roll;
+	}
+	
+	public void show(){
+		System.out.println("Name: "+name);
+		System.out.println("Std: "+std);
+		System.out.println("Roll: "+roll);
+	}
+}
+
+public class Class_Object_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student_1 s1 = new Student_1("Arun","12th",111);
+		s1.show();
+
+	}
+
+}
