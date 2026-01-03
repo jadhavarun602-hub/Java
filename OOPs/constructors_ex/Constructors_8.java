02cb6916d3088f2b06c12dec1bb2eb8a02e48a23
jadhavarun602-hub <jadhavarun602@gmail.com>
2026-01-03 13:41:44 +0530

    Call one constructor from another using this().

--- OOPs/constructors_ex/Constructors_8.java
+++ OOPs/constructors_ex/Constructors_8.java
@@ -35,7 +35,6 @@ public class Constructors_8 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Person p1 = new Person("Female");
 		Person p2 = new Person("Female","Pooja",21,"Pune");
 
 	}
