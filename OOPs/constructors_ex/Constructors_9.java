ea53bd29cecbadcaf2f6b5b9bca5030129db365e
jadhavarun602-hub <jadhavarun602@gmail.com>
2026-01-03 13:49:17 +0530

    Create constructor with no arguments.

---
+++ OOPs/constructors_ex/Constructors_9.java
@@ -0,0 +1,20 @@
+package constructors_ex;
+
+// Create constructor with no arguments.
+
+class Car_1 {
+	
+	Car_1(){
+		System.out.println("Default constructor..");
+	}
+}
+
+public class Constructors_9 {
+
+	public static void main(String[] args) {
+		
+		Car_1 car = new Car_1();
+
+	}
+
+}
