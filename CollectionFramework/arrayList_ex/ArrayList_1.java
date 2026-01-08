349d359db8d680f3c1dbb86cc278fcd6cfbb9157
jadhavarun602-hub <jadhavarun602@gmail.com>
2026-01-08 15:25:35 +0530

    Create an ArrayList and add 5 integer elements to it

---
+++ CollectionFramework/arrayList_ex/ArrayList_1.java
@@ -0,0 +1,28 @@
+package arrayList_ex;
+
+import java.util.ArrayList;
+
+// Create an ArrayList and add 5 integer elements to it.
+
+public class ArrayList_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		
+		arr.add(44);
+		arr.add(87);
+		arr.add(12);
+		arr.add(345);
+		arr.add(99);
+		
+		System.out.print("ArrayList Elements: "+arr);
+		
+//		for(int a : arr) {
+//			System.out.println(a);
+//		}
+
+	}
+
+}
