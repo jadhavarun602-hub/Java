de1ec858ae35717e1a1c371dcebb753e287de19d
jadhavarun602-hub <jadhavarun602@gmail.com>
2026-01-08 15:49:10 +0530

    Find the size of an ArrayList.

---
+++ CollectionFramework/arrayList_ex/ArrayList_4.java
@@ -0,0 +1,22 @@
+package arrayList_ex;
+
+import java.util.ArrayList;
+
+// Find the size of an ArrayList.
+
+public class ArrayList_4 {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> arr = new ArrayList<>();
+		
+		arr.add(56);
+		arr.add(22);
+		arr.add(31);
+		arr.add(98);
+		
+		System.out.println("Array Size: "+arr.size());
+		
+	}
+
+}
