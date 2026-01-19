f76dd2f3e4f867554ebd9e1ecd0c0178168fcfa5
jadhavarun602-hub <jadhavarun602@gmail.com>
2026-01-19 14:11:50 +0530

    Create a HashSet<String> and display all elements

---
+++ CollectionFramework/hashSet_ex/HashSet_2.java
@@ -0,0 +1,26 @@
+package hashSet_ex;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// Create a HashSet<String> and display all elements.
+
+public class HashSet_2 {
+
+	public static void main(String[] args) {
+		
+		Set<String> str = new HashSet<>();
+		
+		str.add("abc");
+		str.add("pqr");
+		str.add("xyz");
+		str.add("rts");
+		str.add("abc");
+		
+		for(String ele : str) {
+			System.out.println(ele);
+		}
+
+	}
+
+}
