eeabc743e924ece1af3574d5ccbbf4d6c91abd52
arun <jadhavarun602@gmail.com>
2026-02-26 22:52:33 +0530

    Create a class without main() and access it from another class

---
+++ OOPs/class_object_ex/Class_18.java
@@ -0,0 +1,9 @@
+package class_object_ex;
+
+public class Class_18 {
+	
+	public void demo() {
+		System.out.println("this is 18th class");
+	}
+
+}
---
+++ OOPs/class_object_ex/Class_Object_18.java
@@ -0,0 +1,14 @@
+package class_object_ex;
+
+// Create a class without main() and access it from another class.
+
+public class Class_Object_18 {
+
+	public static void main(String [] args) {
+		
+		Class_18 c = new Class_18();
+		
+		c.demo();
+	}
+
+}
