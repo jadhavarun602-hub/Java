330918610125a333e5a8cae4037c2b4c0ca0fbda
arun <jadhavarun602@gmail.com>
2026-02-26 21:15:33 +0530

    Create two objects and compare their values.

---
+++ OOPs/class_object_ex/Class_Object_12.java
@@ -0,0 +1,20 @@
+package class_object_ex;
+
+// Create two objects and compare their values.
+
+class Abc{
+	
+}
+
+public class Class_Object_12 {
+
+	public static void main(String[] args) {
+		
+		Abc abc1 = new Abc();
+		Abc abc2 = new Abc();
+		
+		System.out.println(abc1 == abc2);
+
+	}
+
+}
