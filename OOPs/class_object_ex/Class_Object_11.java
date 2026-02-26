98061f0af73a4b91105037cc48ac7843175d74ce
arun <jadhavarun602@gmail.com>
2026-02-26 21:03:43 +0530

    Create a class and initialize variables using object reference

---
+++ OOPs/class_object_ex/Class_Object_11.java
@@ -0,0 +1,20 @@
+package class_object_ex;
+
+// Create a class and initialize variables using object reference.
+
+class MyNumber{
+	int num = 245;
+}
+
+public class Class_Object_11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	MyNumber a = new MyNumber();
+	
+	System.out.println(a.num);
+
+	}
+
+}
