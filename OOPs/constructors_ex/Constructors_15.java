1e292aa3de2e96175644928539d23970e7eb3df0
arun <jadhavarun602@gmail.com>
2026-03-01 18:26:49 +0530

    Create constructor that prints a message.

---
+++ OOPs/constructors_ex/Constructors_15.java
@@ -0,0 +1,20 @@
+package constructors_ex;
+
+// Create constructor that prints a message.
+
+class Message{
+	
+	Message(){
+		System.out.println("Hey i am Constructor..");
+	}
+}
+
+public class Constructors_15 {
+
+	public static void main(String[] args) {
+		
+		Message m = new Message();
+
+	}
+
+}
