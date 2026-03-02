478b5c477edab20ad9c2a155e2e6302449ad7b1e
arun <jadhavarun602@gmail.com>
2026-03-02 13:02:32 +0530

    using the mysql and chek this of executequery and executeupdate using the execute return type of boolean

--- JDBC/jdbc_api/JDBC_8.java
+++ JDBC/jdbc_api/JDBC_8.java
@@ -9,27 +9,26 @@ import java.sql.SQLException;
 public class JDBC_8 {
 
 	public static void main(String[] args) throws SQLException {
-		
-		try(Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/student","root","arun@2004");
-				PreparedStatement stmt = con.prepareStatement("select * from user ;");)
-		{
+
+		try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/student", "root", "arun@2004");
+				PreparedStatement stmt = con.prepareStatement("select * from user ;");) {
 			boolean temp = stmt.execute();
-			if(temp) {
+			if (temp) {
 				System.out.println("DQL");
 				ResultSet rs = stmt.getResultSet();
-				
-				while(rs.next()) {
-					System.out.println("ID: "+rs.getInt(1));
-					System.out.println("Name: "+rs.getString(2));
-					System.out.println("Contact: "+rs.getString(3));
-					System.out.println("Address: "+rs.getString(4));
+
+				while (rs.next()) {
+					System.out.println("ID: " + rs.getInt(1));
+					System.out.println("Name: " + rs.getString(2));
+					System.out.println("Contact: " + rs.getString(3));
+					System.out.println("Address: " + rs.getString(4));
 					System.out.println("--------------------------------");
 				}
-			}else {
+			} else {
 				int row = stmt.getUpdateCount();
-				System.out.println("row affected: "+row);
+				System.out.println("row affected: " + row);
 			}
-		}catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 
