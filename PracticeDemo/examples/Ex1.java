68512ba669917a73dcdb75d83292bbe19d293c46
arun <jadhavarun602@gmail.com>
2026-03-27 12:35:32 +0530

    programs:

---
+++ PracticeDemo/examples/Ex1.java
@@ -0,0 +1,21 @@
+package examples;
+
+
+public class Ex1 {
+	
+  static int num = 70;
+	
+	public static void main(String[] args) {
+		
+
+		Ex1 ex = new Ex1();
+		Ex1 ex2 = new Ex1();
+		
+		ex.num = 20;
+		
+		System.out.println(ex.num);
+		
+		
+	}
+
+}
